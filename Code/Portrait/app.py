1426a5407a93bb6adf9261ed8e42796fa807d0e1
Sarah Duncan <duncanbdunkin@gmail.com>
2013-05-13 21:37:10 -0400

    working on designs

--- Code/Portrait/app.py
+++ Code/Portrait/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.rout('/main/',methods=["POST", "GET"])
+def main():
+    return render_template("main.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
