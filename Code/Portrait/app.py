c65925ce5f68315ce47c4fe214ea3f34b7ca1563
Sarah Duncan <duncanbdunkin@gmail.com>
2013-05-18 16:58:42 -0400

    ehhhhh help

--- Code/Portrait/app.py
+++ Code/Portrait/app.py
@@ -16,9 +16,26 @@ def home():
 @app.route('/main',methods=["POST", "GET"])
 def main():
     if request.method == "POST":
+        image = request.form["pic"]
         button = request.form["button"]
+
         if button == "Log Out":
             return redirect(url_for("home"))
+        if image == "goal":
+            return redirect(url_for("goal"))
+        if image == "music":
+            return redirect(url_for("music"))
+        if image == "bench":
+            return redirect(url_for("bench"))
+        if image == "baseball":
+            return redirect(url_for("baseball"))
+        if image == "code":
+            return redirect(url_for("code"))
+        if image == "rose":
+            return redirect(url_for("rose"))
+        if image == "bandana":
+            return redirect(url_for("bandana"))
+
     return render_template("main.html")
 
 @app.route('/goal',methods=["POST", "GET"])
@@ -30,6 +47,30 @@ def goal():
         if button == "Main":
             return redirect(url_for("main"))
     return render_template("goal.html")
+
+@app.route('/music',methods=["POST", "GET"])
+def music():
+    return render_template("music.html")
+
+@app.route('/bench',methods=["POST", "GET"])
+def music():
+    return render_template("bench.html")
+
+@app.route('/baseball',methods=["POST", "GET"])
+def music():
+    return render_template("baseball.html")
+
+@app.route('/code',methods=["POST", "GET"])
+def music():
+    return render_template("code.html")
+
+@app.route('/rose',methods=["POST", "GET"])
+def music():
+    return render_template("rose.html")
+
+@app.route('/bandana',methods=["POST", "GET"])
+def music():
+    return render_template("bandana.html")
 
 if __name__ == "__main__":
     app.debug = True
